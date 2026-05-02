11b5683e43a086d53b34d7757e9f6e864636fc7b
lilico7 <xxtamong@gmail.com>
2026-05-02 18:06:48 +0100

    Final merge from lili branch with all features

---
+++ L4 code/L4 - Code/12. query examples.py
@@ -0,0 +1,29 @@
+'''
+If done locally, remember to input mysql -u root -p in the terminal
+If MySQL does not work, export PATH=${PATH}:/usr/local/mysql/bin
+Let us do some queries on the database
+'''
+from sqlalchemy import create_engine
+import dbinfo
+import datetime
+
+engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(
+    dbinfo.DB_USER, dbinfo.DB_PASS, dbinfo.DB_URI, dbinfo.DB_PORT, dbinfo.DB_NAME
+))
+
+# 1. Check total number of static stations
+res = engine.execute("SELECT COUNT(*) FROM station;").fetchone()
+print(f"Total stations in 'station' table: {res[0]}")
+
+# 2. Check total number of dynamic records collected
+res_avail = engine.execute("SELECT COUNT(*) FROM availability;").fetchone()
+print(f"Total records in 'availability' table: {res_avail[0]}")
+
+# 3. Show the latest 5 entries to confirm time sync
+print("\n--- Latest 5 Availability Records ---")
+latest = engine.execute("SELECT * FROM availability ORDER BY last_update DESC LIMIT 5;").fetchall()
+for row in latest:
+    print(row)
+
+# 4. ...
+
---
+++ create-db-weather.py
@@ -0,0 +1,63 @@
+import dbinfo
+import requests
+import json
+import sqlalchemy as sqla
+from sqlalchemy import create_engine
+import traceback
+import glob
+import os
+from pprint import pprint
+import simplejson as json
+import time
+from IPython.display import display
+
+connection_string = "mysql+pymysql://{}:{}@{}:{}/{}".format(dbinfo.DB_USER, dbinfo.DB_PASS, dbinfo.DB_URI, dbinfo.DB_PORT, dbinfo.DB_NAME)
+
+engine = create_engine(connection_string, echo = True)
+
+for res in engine.execute("SHOW VARIABLES;"):
+    print(res)
+
+# Let us create a simplified JCDecaux table: ADD ALL YOUR VARIABLES!
+# VARCHAR(256) indicates a string with max 256 chars
+
+sql_current = '''
+CREATE TABLE IF NOT EXISTS station (
+number INTEGER PRIMARY KEY,
+address VARCHAR(256), 
+banking INTEGER,
+bikestands INTEGER,
+name VARCHAR(256),
+status VARCHAR(256),
+position_lat FLOAT,
+position_lng Float,
+bonus INTEGER,
+overflow INTEGER);
+'''
+
+# Execute the query
+res = engine.execute(sql)
+
+# Use the engine to execute the DESCRIBE command to inspect the table schema
+tab_structure = engine.execute("SHOW COLUMNS FROM station;")
+
+# Fetch and print the result to see the columns of the table
+columns = tab_structure.fetchall()
+print(columns)
+
+##################CREATE AVAILABILITY TABLE: DO NOT FORGET ALL VARIABLES############
+sql = """
+CREATE TABLE IF NOT EXISTS availability (
+number INTEGER,
+available_bikes INTEGER,
+available_bike_stands INTEGER,
+last_update DATETIME,
+status VARCHAR(16),
+mechanical_bikes INTEGER,
+electrical_bikes INTEGER,
+total_bike_stands INTEGER,
+PRIMARY KEY (number, last_update)
+);
+"""
+
+engine.execute(sql)
